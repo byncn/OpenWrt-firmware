68c30eeb0bc1b72c2b48abc6ae95c27b92cee527
BYN <38535696+byncn@users.noreply.github.com>
2022-01-08 23:07:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 #
 
 # 设置默认IP为 192.168.31.1
-#sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
