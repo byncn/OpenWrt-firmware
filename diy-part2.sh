d231ee78fdcfa2ba082a55f27b91f0bfd39a1695
BYN <38535696+byncn@users.noreply.github.com>
2022-01-08 23:06:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 设置默认IP为 192.168.31.1
+#sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
