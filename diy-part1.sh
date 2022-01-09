d95ffb480fbee39b61322c109e3121bd4b1f062f
BYN <38535696+byncn@users.noreply.github.com>
2022-01-09 13:15:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git smpackage https://github.com/kenzok78/small-package' >>feeds.conf.default
